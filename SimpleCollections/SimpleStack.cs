36b2b3d1ae0090dbd46bef6aeb15186395a74d8b
V1ntester <valentin_n_valen@mail.ru>
2025-12-21 14:46:10 +0300

    (+) Added SimpleStack

--- SimpleCollections/SimpleStack.cs
+++ SimpleCollections/SimpleStack.cs
@@ -10,7 +10,7 @@ public class SimpleStack<T> : SimpleList<T>
     public T Pop()
     {
         if (Length == 0) throw new InvalidOperationException("Stack is empty");
-        
+
         T value = this[Length - 1];
         RemoveAt(Length - 1);
         return value;
